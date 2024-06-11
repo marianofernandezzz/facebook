aa76d54b6b08333bf7c93d8e2a1228cd4ec915df
marianofernandezzz <exequiel11manda@gmail.com>
2024-06-10 21:50:30 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,21 +5,17 @@ const userService = require('../data/userService');
 
 
 const userController = {
+  login: (req, res) => {
+      res.render('products/index');
+  },
 
-    login: (req, res) => {
-        res.render('products/index');
-      },
-
-      processRegister: async (req, res) => {
-        try {
-        
-           await userService.create(req);
-           res.redirect('../../')
-
-        } catch (error) {
-          res.status(500).send({ error: 'hubo un error al registrarse' });
-        }
-      },
-
+  processRegister: async (req, res) => {
+      try {
+          await userService.create(req);
+          res.redirect('../../');
+      } catch (error) {
+          res.status(500).send({ error: 'Hubo un error al registrarse' });
+      }
+  },
 }
 module.exports = userController
