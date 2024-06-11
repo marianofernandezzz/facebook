c16be84dd3eb5d846261499b4f76ee2b16200002
marianofernandezzz <exequiel11manda@gmail.com>
2024-06-10 21:51:29 -0300

    Update userService.js

--- src/data/userService.js
+++ src/data/userService.js
@@ -4,21 +4,17 @@ const db = require('../model/database/models');
 
 
 const userService = {
-
     create: async function (req) {
         try {
-            
             let newUser = await db.User.create({
-               
                 email: req.body.email,
                 password: req.body.password,
             })
-            return newUser
+            return newUser;
         } catch (error) {
-
+            return Promise.reject(error);
         }
-    },
-
+    }
 }
 
-module.exports = userService;
+module.exports = userService
